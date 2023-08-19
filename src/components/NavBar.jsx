0105f8128a1098fabc8f066dfa883afe839b6d3c
ZakiaHussaini <zakiahussaini19@gmail.com>
2023-08-19 15:40:50 +0430

    used use context hook to share login user between components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        to="/signup"
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
   return (
     <div>
     <Navbar expand="md" fixed='top' className={styles.NavBar}>
@@ -18,12 +40,7 @@ const NavBar = () => {
             <NavLink to='/' exact className={styles.NavLink} activeClassName={styles.Active} >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink to='signin'  className={styles.NavLink} activeClassName={styles.Active}> 
-              <i className="fas fa-sign-in-alt"></i>Sign in
-            </NavLink>
-            <NavLink to='signup'  className={styles.NavLink} activeClassName={styles.Active}> 
-              <i className="fas fa-user-plus"></i>Sign up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
